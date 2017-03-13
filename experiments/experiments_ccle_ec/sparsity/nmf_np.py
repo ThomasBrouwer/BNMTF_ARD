2e9eeaeab72b4ad5fed880c2fa2cf83b8250a5ef
ThomasBrouwer <tab43@cam.ac.uk>
2017-03-13 12:24:25 +0100

    Prepared sparsity tests CCLE for Nemo machine.

--- experiments/experiments_ccle_ec/sparsity/nmf_np.py
+++ experiments/experiments_ccle_ec/sparsity/nmf_np.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 
 ''' Experiment settings. '''
 repeats = 10
-fractions_unknown = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9] # [0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9]
+fractions_unknown = [0.4, 0.5, 0.6, 0.7, 0.8] # [0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9]
 
 output_folder = './results/'
 output_file = output_folder+'nmf_np.txt'
